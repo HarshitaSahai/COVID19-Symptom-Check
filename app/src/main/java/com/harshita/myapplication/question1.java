c887d0b3a06d5bcee28019559b230953ed69840c
KhyatiMehta3 <khyati@atom360.io>
2020-06-28 17:15:32 +0530

    WIP: new questions popping up, got rid of null pointer issue. Replace all JSONObject[] with JSONArray for simplicity

--- app/src/main/java/com/harshita/myapplication/question1.java
+++ app/src/main/java/com/harshita/myapplication/question1.java
@@ -3,24 +3,17 @@ package com.harshita.myapplication;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.VolleyError;
@@ -30,15 +23,9 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.sql.Array;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.MutableLiveData;
@@ -53,24 +40,15 @@ import com.harshita.myapplication.models.ChatMessage;
 import com.harshita.myapplication.views.ChatView;
 
 import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.util.List;
 import java.util.Objects;
-import java.util.concurrent.TimeUnit;
 
 import static com.harshita.myapplication.models.ChatMessage.Type.RECEIVED;
 import static com.harshita.myapplication.models.ChatMessage.Type.SENT;
 
 public class question1  extends AppCompatActivity implements View.OnClickListener {
 
-
-    //WebView wv ;
-    //String url = "https://covid-19.ada.com";
-    //JSONObject obj;
     private ChatView chatView1;
-    private int questionIndex =0;
     private JSONObject covidObject = new JSONObject();
     private JSONObject apiResponse = new JSONObject();
     private MutableLiveData<JSONObject> responseAlert = new MutableLiveData<>();
@@ -86,21 +64,6 @@ public class question1  extends AppCompatActivity implements View.OnClickListene
 
         chatView1.addMessage(new ChatMessage("Please Select Your Gender",System.currentTimeMillis(), RECEIVED));
         chatView1.addMessage(new ChatMessage(question_1(), System.currentTimeMillis(), SENT));
-        chatView1.setOnSentMessageListener(new ChatView.OnSentMessageListener() {
-            @Override
-            public boolean sendMessage(ChatMessage chatMessage) {
-                //TODO: this function is still incomplete, need to think through the implementation
-                switch(questionIndex){
-                    case 0:
-                        return true;
-
-                    case 1:
-
-                }
-                return false;
-            }
-
-        });
         chatView1.setOnClickListener(this);
 
         responseAlert.observe(this, new Observer<JSONObject>() {
@@ -116,15 +79,9 @@ public class question1  extends AppCompatActivity implements View.OnClickListene
         LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         return inflater.inflate(R.layout.sample_layout, null);
     }
- /* private View question_2(){
-        //Sample view given here
-        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        return inflater.inflate(R.layout.question2, null);
-    }*/
 
     private void displayQuestions(){
         WorkManager workManager = WorkManager.getInstance(this);
-        //Add your API response just like how you did it for intents. -> Convert to String
         Data inputData = new Data.Builder().putString("apiResponse",apiResponse.toString()).build();
 
         @SuppressLint("RestrictedApi") OneTimeWorkRequest displayNextQuestionRequest =
@@ -159,8 +116,6 @@ public class question1  extends AppCompatActivity implements View.OnClickListene
         });
 
     }
-    //This is a sample api response.
-    // TODO : Use this function to call the actual api and return the response
     private void getAPIJson(){
         try {
             RequestQueue queue = Volley.newRequestQueue(this);
@@ -169,7 +124,7 @@ public class question1  extends AppCompatActivity implements View.OnClickListene
 
             JsonObjectRequest getRequest = new JsonObjectRequest(
                     Request.Method.POST
-                    ,  url,covidObject, new com.android.volley.Response.Listener<JSONObject>() {
+                    ,url,covidObject, new com.android.volley.Response.Listener<JSONObject>() {
                 @Override
                 public void onResponse(JSONObject response)
                 {
@@ -189,8 +144,6 @@ public class question1  extends AppCompatActivity implements View.OnClickListene
 
                 }
             }){
-
-
                 @Override
                 public Map<String, String> getHeaders()
                 {
@@ -204,7 +157,6 @@ public class question1  extends AppCompatActivity implements View.OnClickListene
             };
            //return getRequest;
             queue.add(getRequest);
-            //return covidObject;
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -216,67 +168,32 @@ public class question1  extends AppCompatActivity implements View.OnClickListene
             final JSONArray itemsArray = new JSONArray(items);
             final LinearLayout checkboxHolder = new LinearLayout(this);
             checkboxHolder.setOrientation(LinearLayout.VERTICAL);
-            final CheckBox[] cbs; // Checkbox array
-            cbs = new CheckBox[itemsArray.length()]; // Size of array
+
             Button bgmp = new Button(this);
             bgmp.setText("Next"); // Button for storing value in evidence[]
 
-            for(int i=0;i<itemsArray.length();i++)
-            {
-                cbs[i] = new CheckBox(this); //here i is iteration variable
-                cbs[i].setText(itemsArray.getJSONObject(i).getString("name")); // Storing content
-                checkboxHolder.addView(cbs[i]);
+            for(int i=0; i<itemsArray.length();i++){
+                CheckBox selectableCheckBox = new CheckBox(this);
+                selectableCheckBox.setText(itemsArray.getJSONObject(i).getString("name"));
+                checkboxHolder.addView(selectableCheckBox,i);
             }
-            checkboxHolder.addView(bgmp); // Adding button to view
 
-
-            /*checkboxHolder.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Toast.makeText(question1.this, "Clicked pa clicked :laugh", Toast.LENGTH_SHORT).show();
-                }
-            });*/
-
+            checkboxHolder.addView(bgmp,itemsArray.length()); // Adding button to view
 
             bgmp.setOnClickListener( new View.OnClickListener() {
+
                 @Override
                 public void onClick(View v) {
-                    JSONObject cjo[ ] = new  JSONObject[itemsArray.length()];// Array to store current checbox value (question4 line 55 - 59)
+                    for(int i=0;i<itemsArray.length();i++){
+                        JSONObject evidence_subJson = new JSONObject();
+                        try{
+                            evidence_subJson.put("id",itemsArray.getJSONObject(i).getString("id"));
+                            evidence_subJson.put("choice_id",((CheckBox)checkboxHolder.getChildAt(i)).isChecked()? "present": "absent");
 
-                    for(int i = 0;i<itemsArray.length();i++)
-                    {
-                        // TODO : cjo[] referring to null object prevent that
+                            evidence.put(evidence_subJson);
 
-                        if(cbs[i].isChecked()){
-                            try {
-                                cjo[i].put("id",itemsArray.getJSONObject(i).getString("id")); // Getting the id from item
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                            try {
-                                cjo[i].put("choice","present"); // Storing present if checked
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }else{
-                            try {
-                                cjo[i].put("id",itemsArray.getJSONObject(i).getString("id")); // Getting the id for item
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                            try {
-                                cjo[i].put("choice","absent"); // Storing absent if not checked
-
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-
-                        }
-                        evidence.put(cjo[i]); // Appending the value in evidence "Can also be done after stroing value in every cjo" (question 4 line 77)
-
+                        }catch (Exception e){e.printStackTrace();}
                     }
-
-                    questionIndex+=1;
                     // question4 line 209
                     try {
                         covidObject.put("evidence",evidence);
@@ -301,11 +218,9 @@ public class question1  extends AppCompatActivity implements View.OnClickListene
         //TODO: Need to write this view, with Radio group as only one of the options should be selected
         try {
             JSONArray itemsArray = new JSONArray(items);
-            LinearLayout ll = new LinearLayout(this);
-            //RadioGroup radiogroup = new RadioGroup(this);
-            //radiogroup.setOrientation(ll.VERTICAL);
             RadioGroup radiobuttonHolder = new RadioGroup(this);
-            radiobuttonHolder.setOrientation(ll.VERTICAL);
+            radiobuttonHolder.setOrientation(LinearLayout.VERTICAL);
+
             for(int i=0; i<itemsArray.length();i++){
                 RadioButton selectableRadioButton = new RadioButton(this);
                 selectableRadioButton.setText(itemsArray.getJSONObject(i).getString("name"));
@@ -330,9 +245,9 @@ public class question1  extends AppCompatActivity implements View.OnClickListene
     public void onClick(View v) {
         try{
             switch (v.getId()){
-                case R.id.toq2:
-                    RadioButton m = (RadioButton)findViewById(R.id.male);
-                    RadioButton f = (RadioButton)findViewById(R.id.female);
+                case R.id.genderNextButton:
+                    RadioButton m =findViewById(R.id.male);
+                    RadioButton f =findViewById(R.id.female);
                     if(m.isChecked()){
                     covidObject.put("sex","male");
                     covidObject.put("age",21);
@@ -346,13 +261,7 @@ public class question1  extends AppCompatActivity implements View.OnClickListene
 
                     }
                     break;
-
-
-
-
             }
-
-            questionIndex+=1;
             getAPIJson();
         }catch (Exception e){e.printStackTrace();}
     }
